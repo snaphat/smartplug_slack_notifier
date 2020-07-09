535f9584d431c5b1b0b4965300e64d33bd98ae69
Noritaa <aaron.landwehr@chipdesignsystems.com>
2020-07-09 00:00:13 -0400

    Print error state change notifications. Remember alias in error state. Don't return errors from send_slack_message().

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,109 @@
 use hs100api::SmartPlug;
 use slack_api::sync as slack;
-use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
 // Send slack message
-fn send_slack_message(channel: &str, text: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let token = "***REMOVED***";
-    let client = slack::default_client().map_err(|_| "Could not get default_client")?;
+fn send_slack_message(channel: &str, text: &str) {
+    let send_msg = || -> Result<(), Box<dyn std::error::Error>> {
+        let token = "***REMOVED***";
+        let client = slack::default_client().map_err(|_| "Could not get default_client")?;
 
-    let response = slack::chat::post_message(
-        &client,
-        &token,
-        &slack::chat::PostMessageRequest {
-            channel: channel,
-            text: text,
-            username: Some("smartplug_notifier"),
-            ..slack::chat::PostMessageRequest::default()
-        },
-    );
+        slack::chat::post_message(
+            &client,
+            &token,
+            &slack::chat::PostMessageRequest {
+                channel: channel,
+                text: text,
+                username: Some("smartplug_notifier"),
+                ..slack::chat::PostMessageRequest::default()
+            },
+        )?;
 
-    if let Ok(response) = response {
-        if let Some(message) = response.message {
-            println!("Got {:?} message:", message);
-        }
-    } else {
-        println!("Error message: {:?}", response);
+        Ok(())
+    };
+
+    if let Err(err) = send_msg() {
+        println!("Error message: {:?}", err);
     }
-    Ok(())
+}
+
+struct Host {
+    ip: String,
+    plug: SmartPlug,
+    state: Option<i64>,
+    alias: String,
 }
 
 fn main() {
     // Add hosts to query:
-    let hosts = [
+    let ips = [
         //"192.168.2.147:9999", // Clab-room-light
         "192.168.2.146:9999", // SC6800-NSLEDS1
         "192.168.2.101:9999", // SC6800-NSLEDS3
     ];
 
     // Populate plugs
-    let mut entry: HashMap<String, bool> = HashMap::new();
-    let mut plugs: Vec<SmartPlug> = Vec::new();
-    for host in hosts.iter() {
-        plugs.push(SmartPlug::new(host));
+    let mut hosts: Vec<Host> = Vec::new();
+    for ip in &ips {
+        hosts.push(Host {
+            ip: ip.to_string(),
+            state: None, // empty state.
+            plug: SmartPlug::new(ip),
+            alias: String::from("Unk"),
+        });
     }
 
     // Loop forever
     loop {
         // Loop each plug
-        let mut i = 0;
-        for plug in plugs.iter() {
-            // Query plug info
-            let info = plug.sysinfo();
-            let info = match info {
+        for host in &mut hosts {
+            // Query plug info or None on error
+            let info = match host.plug.sysinfo() {
                 Ok(info) => info,
-                Err(_err) => {
-                    println!("Host: {}, Error getting plug info!", hosts[i]);
-                    continue;
-                }
+                Err(_err) => (hs100api::types::PlugInfo { system: None, emeter: None }),
             };
 
-            // Retrieve info from the plug.
-            let (mac, alias, relay_state) = match info.system {
-                Some(sys) => (sys.get_sysinfo.mac, sys.get_sysinfo.alias, sys.get_sysinfo.relay_state != 0),
-                None => {
-                    println!("Host: {}, Error decoding plug info!", hosts[i]);
-                    continue;
-                }
+            // Retrieve info from the plug or -1 on error.
+            let (alias, mac, state) = match info.system {
+                Some(sys) => (Some(sys.get_sysinfo.alias), sys.get_sysinfo.mac, sys.get_sysinfo.relay_state),
+                None => (None, String::from("Unk"), -1),
             };
 
-            // Insert current state into map and return old state.
-            let old_val = entry.insert(mac.clone(), relay_state).unwrap_or(relay_state);
+            // Insert alias if it exists otherwise use original (remembers alias in the event of error).
+            if let Some(alias) = alias {
+                host.alias = alias;
+            }
+
+            // Insert current state and return old state.
+            let old_state = host.state.replace(state).unwrap_or(state);
+
+            // Whether a state change has occurred.
+            let changed = old_state != state;
 
             // If state change send message.
-            let changed = if old_val != relay_state {
+            if changed {
                 // Populate info for printing.
-                let (state, icon) = if relay_state == true {
-                    ("ON", ":red_circle:")
-                } else {
-                    ("OFF", ":large_blue_circle:")
+                let (state, icon) = match state {
+                    1 => ("On", ":red_circle:"),
+                    0 => ("Off", ":large_blue_circle:"),
+                    _ => ("Err", ":warning:"), // Error state.
                 };
 
                 // Format string.
-                let msg = format!("{}{} *{}* switched *{}*! {}{}", icon, icon, &alias, state, icon, icon);
+                let msg = format!("{}{} *{}* switched *{}*! {}{}", icon, icon, host.alias, state, icon, icon);
 
                 // Send message.
-                if let Err(err) = send_slack_message("#remote_operation", &msg) {
-                    println!("{}", err);
-                }
-
-                true // return changed.
-            } else {
-                false // return unchanged.
-            };
+                send_slack_message("#remote_operation", &msg);
+            }
 
             // Print device state
             println!(
-                "Host: {}, MAC: {}, Device: {}, Relay State: {}, Changed {}",
-                hosts[i], mac, alias, relay_state, changed
+                "Host: {}, Alias: {}, MAC: {}, Relay State: {}, Changed {}",
+                host.ip, host.alias, mac, state, changed
             );
-
-            // Sleep before querying again.
-            i += 1;
         }
+        // Sleep before querying again.
         thread::sleep(Duration::from_secs(30));
         println!("========");
     }
